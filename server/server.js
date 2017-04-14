558d3b6d19780faf921606d19122992372dfef3d
Christophe DUFOUR <christophedufour@iMac-de-Christophe-DUFOUR-2.local>
2017-04-14 12:56:59 +0200

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -20,6 +20,14 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find({}).then((todos) => {
+    res.send({todos}); // todos array rappend in object for future more flexible
+  }, (err) => {
+    res.status(400).send(err);
+  });
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
