e55296a90da8893a2a22d4c48df3892eaf57fd0c
Christophe DUFOUR <christophedufour@iMac-de-Christophe-DUFOUR-2.local>
2017-04-15 09:09:59 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -23,6 +24,23 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
   Todo.find({}).then((todos) => {
     res.send({todos}); // todos array rappend in object for future more flexible
+  }, (err) => {
+    res.status(400).send(err);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById({_id: id}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
   }, (err) => {
     res.status(400).send(err);
   });
