30b24e1beff72d07afd44779fca02be42583c18f
Christophe DUFOUR <christophedufour@iMac-de-Christophe-DUFOUR-2.local>
2017-04-16 14:56:15 +0200

    Add Get /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -111,6 +112,42 @@ app.post('/users', (req, res) => {
 
 });
 
+// var authenticate = (req, res, next) => {
+//   var token = req.header('x-auth');
+//
+//   User.findByToken(token).then((user) => {
+//     if (!user) {
+//       return Promise.reject();
+//     }
+//
+//     //res.send(user);
+//     req.user = user;
+//     req.token = token;
+//     next();
+//
+//   }).catch((err) => {
+//     res.status(401).send();
+//   });
+// };
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+});
+
+// private route first example
+// app.get('/users/me', (req, res) => {
+//   var token = req.header('x-auth');
+//
+//   User.findByToken(token).then((user) => {
+//     if (!user) {
+//       return Promise.reject();
+//     }
+//
+//     res.send(user);
+//   }).catch((err) => {
+//     res.status(401).send();
+//   });
+// });
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
